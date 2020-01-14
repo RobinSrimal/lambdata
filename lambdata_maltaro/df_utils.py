fd7e60aa60d46ee60221ddd37ae22b3c52d87c80
Robin Srimal <robin.srimal@gmail.com>
2020-01-14 20:21:14 +0100

    checked with autopep8

--- lambdata_maltaro/df_utils.py
+++ lambdata_maltaro/df_utils.py
@@ -5,15 +5,23 @@ utility functions for working with DataFrames
 import pandas
 import numpy
 
-TEST_DF = pandas.DataFrame([1,2,3])
+TEST_DF = pandas.DataFrame([1, 2, 3])
 
 
-def add_list_to_dataframe(mylist,df):
+def add_list_to_dataframe(mylist, df):
+    """
+    Adds a list to pandas dataframe as a new column. Then returns the extended
+    dtaframe.
+
+    """
     df["new_column"] = mylist
     return df
 
 
 def count_zeros(df):
-    num_zeros = (df.values==0).sum()
-    print("Your Dataframe contains this amount of zeros:", num_zeros)
-    
+    """
+    Transforms a dataframe to a np array, then counts how many 0s are in the
+    array. The result will be printed out.
+    """
+    num_zeros = (df.values == 0).sum()
+    print("Your Dataframe contains {} zeros".format(num_zeros))
