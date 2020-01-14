8bc4130f64bc482e2b0d679d2b91294b0abe21e2
Robin Srimal <robin.srimal@gmail.com>
2020-01-14 17:21:40 +0100

    functions added

--- lambdata_maltaro/df_utils.py
+++ lambdata_maltaro/df_utils.py
@@ -3,5 +3,17 @@ utility functions for working with DataFrames
 """
 
 import pandas
+import numpy
 
 TEST_DF = pandas.DataFrame([1,2,3])
+
+
+def add_list_to_dataframe(mylist,df):
+    df["new_column"] = mylist
+    return df
+
+
+def count_zeros(df):
+    num_zeros = (df.values==0).sum()
+    print("Your Dataframe contains this amount of zeros:", num_zeros)
+    
